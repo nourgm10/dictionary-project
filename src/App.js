b24c82a37b609433ea276415c45ba4d2f03031e1
nourgm10 <nourgm10@gmail.com>
2024-05-22 18:03:55 +0200

    created search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container-fluid">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="fixed-bottom text-center">
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/nourlivia/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Nour Ghallale
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/nourgm10/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <h2>What word would you like to look up?</h2>
+      <form onSubmit={search}>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Type a word..."
+              autoFocus="on"
+              onChange={handleKeywordChange}
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-dark w-100 button"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
