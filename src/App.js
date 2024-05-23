4dbe0f2bc6e9014e9bfa713a4ba173db03dab06c
nourgm10 <nourgm10@gmail.com>
2024-05-23 12:00:06 +0200

    fixed footer

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <main>
           <Dictionary />
         </main>
-        <footer className="sticky-bottom text-center">
+        <footer className="fixed-bottom sticky-bottom text-center">
           This project was coded by{" "}
           <a
             href="https://www.linkedin.com/in/nourlivia/"
