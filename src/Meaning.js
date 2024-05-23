3b541dfe9112e0d09dda004091d8839700cf0f5b
nourgm10 <nourgm10@gmail.com>
2024-05-23 14:49:06 +0200

    fixed appearance

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,23 +3,20 @@ import Synonyms from "./Synonyms";
 import "./Meaning.css";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
+  const firstDefinition = props.meaning.definitions[0];
+
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              <span>&#8226; </span>
-              {definition.definition}
-              <br />
-              <p className="example">{definition.example}</p>
-              <Synonyms synonyms={definition.synonyms} />{" "}
-            </p>
-          </div>
-        );
-      })}
+      <div>
+        <p>
+          <span>&#8226; </span>
+          {firstDefinition.definition}
+          <br />
+          <p className="example">"{firstDefinition.example}"</p>
+          <Synonyms synonyms={firstDefinition.synonyms} />{" "}
+        </p>
+      </div>
     </div>
   );
 }
